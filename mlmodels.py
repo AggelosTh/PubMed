26d52777296b6cfb0a564e2ccdcf51135e185b0c
AggelosTh <aggtheo@hotmail.com>
2024-10-07 01:14:11 +0300

    Clean up the SBert model

--- mlmodels.py
+++ mlmodels.py
@@ -1,34 +1,21 @@
 import torch.nn as nn
 import torch
-# from transformers import AutoTokenizer, AutoModel
-# tokenizer = AutoTokenizer.from_pretrained("medicalai/ClinicalBERT")
-# model = AutoModel.from_pretrained("medicalai/ClinicalBERT")
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f'Using device: {device}')
 
 from sentence_transformers import SentenceTransformer
-sbert = SentenceTransformer('pritamdeka/S-PubMedBert-MS-MARCO', device=device)
+sbert = SentenceTransformer('all-MiniLM-L6-v2', device=device)
+sbert.max_seq_length = 256
 
 class SBertModel(nn.Module):
     def __init__(self, input_dimension, num_of_labels):
         super(SBertModel, self).__init__()
-        # self.dropout5 = nn.Dropout(0.5)
-        # self.dropout3 = nn.Dropout(0.3)
-        # self.dropout2 = nn.Dropout(0.2)
-        # self.dropout1 = nn.Dropout(0.1)
 
         self.fc1 = nn.Linear(input_dimension, 5000)
-        # self.bn1 = nn.BatchNorm1d(5000)
-
         self.fc2 = nn.Linear(5000, 2000)
-        # self.bn2 = nn.BatchNorm1d(2000)
-
         self.fc3 = nn.Linear(2000, 800)
-        # self.bn3 = nn.BatchNorm1d(800)
-
         self.fc4 = nn.Linear(800, 100)
-        # self.bn4 = nn.BatchNorm1d(100)
-
         self.fc5 = nn.Linear(100, num_of_labels)
 
         self.gelu = nn.GELU()
