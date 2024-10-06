2a830963402d1e77f1b8f2878de8e8124b60ad84
AggelosTh <aggtheo@hotmail.com>
2024-10-07 01:47:14 +0300

    Fix the training process for multilabel binary classification

--- train.py
+++ train.py
@@ -11,30 +11,29 @@ from torch.optim.lr_scheduler import ReduceLROnPlateau
 X = df['text'].tolist()
 y = df['labels'].tolist()
 
-num_of_labels = 14
+num_of_labels = len(y[0])
 
 X_train_text, X_temp_text, y_train, y_temp = train_test_split(X, y, test_size=0.2, random_state=42)
 X_dev_text, X_test_text, y_dev, y_test = train_test_split(X_temp_text, y_temp, test_size=0.5, random_state=42)
 
 print("Encoding the texts ...")
 x_train = sbert.encode(
-    [sentence for sentence in X_train_text]
+    [sentence for sentence in X_train_text], device=device
 )
 x_dev = sbert.encode(
-    [sentence for sentence in X_dev_text]
+    [sentence for sentence in X_dev_text], device=device
 )
 x_test = sbert.encode(
-    [sentence for sentence in X_test_text]
+    [sentence for sentence in X_test_text], device=device
 )
 
 input_dimension = x_train.shape[1]
-model = SBertModel(input_dimension, num_of_labels)
 
 print("Convert the data to tensors")
 x_train_tensor = torch.tensor(x_train, dtype=torch.float32).to(device)
-y_train_tensor = torch.tensor(y_train, dtype=torch.long).to(device)
+y_train_tensor = torch.tensor(y_train, dtype=torch.float32).to(device)
 x_dev_tensor = torch.tensor(x_dev, dtype=torch.float32).to(device)
-y_dev_tensor = torch.tensor(y_dev, dtype=torch.long).to(device)
+y_dev_tensor = torch.tensor(y_dev, dtype=torch.float32).to(device)
 
 train_dataset = TensorDataset(x_train_tensor, y_train_tensor)
 train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
@@ -42,9 +41,10 @@ train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
 val_dataset = TensorDataset(x_dev_tensor, y_dev_tensor)
 val_loader = DataLoader(val_dataset, batch_size=32, shuffle=True)
 
+print("Initialize model")
 model = SBertModel(input_dimension, num_of_labels).to(device)
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.0001)
+criterion = nn.BCEWithLogitsLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.00001)
 scheduler = ReduceLROnPlateau(optimizer, mode='min', patience=5, factor=0.5)
 
 best_val_acc = 0
@@ -65,8 +65,8 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
         running_loss += loss.item()
-        _, predicted = torch.max(outputs, 1)
-        total_train += labels.size(0)
+        predicted = (torch.sigmoid(outputs) > 0.5).float()
+        total_train += labels.numel()
         correct_train += (predicted == labels).sum().item()
         train_loader_tqdm.set_postfix(training_loss=running_loss / len(train_loader))
     
@@ -74,30 +74,34 @@ for epoch in range(num_epochs):
     train_acc = correct_train / total_train
     
     val_loss = 0
-    model.eval()
     correct = 0
     total = 0
 
-    print(f'Epoch {epoch+1}/{num_epochs}, Training Loss: {train_loss}, Training Accuracy: {train_acc}')
+    print(f'Epoch {epoch+1}/{num_epochs}, Training Loss: {train_loss}, Training Accuracy: {train_acc:.4f}')
     
+    model.eval()
     val_loader_tqdm = tqdm(val_loader, desc=f'Epoch {epoch+1}/{num_epochs}', unit='batch')
     
     with torch.no_grad():
         for inputs, labels in val_loader_tqdm:
+            inputs, labels = inputs.to(device), labels.to(device)
+            
             outputs = model(inputs)
             loss = criterion(outputs, labels)
             val_loss += loss.item()
 
-            _, predicted = torch.max(outputs, 1)
-            total += labels.size(0)
+            predicted = (torch.sigmoid(outputs) > 0.5).float()
             correct += (predicted == labels).sum().item()
+            total += labels.numel()
             val_loader_tqdm.set_postfix(accuracy=correct / total)
     
     val_loss /= len(val_loader)
     val_acc = correct / total
     scheduler.step(val_loss)
-    print(f'Epoch {epoch+1}/{num_epochs}, Val Loss: {val_loss}, Val Accuracy: {val_acc}')
+    
+    print(f'Epoch {epoch+1}/{num_epochs}, Val Loss: {val_loss}, Val Accuracy: {val_acc:.4f}')
 
     if val_acc > best_val_acc:
         best_val_acc = val_acc
         best_model_wts = model.state_dict().copy()
+        torch.save(best_model_wts, 'best_model.pth')
