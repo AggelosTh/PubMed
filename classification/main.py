646714a1c8c61270b3674f0d560bd87b147a1b83
AggelosTh <aggtheo@hotmail.com>
2024-10-10 22:57:45 +0300

    Add docstrings

--- classification/main.py
+++ classification/main.py
@@ -4,11 +4,7 @@ import eda_utils
 from load_data import df
 from mlmodel import SBertModel, sbert, device
 import torch
-import logging
 from contextlib import asynccontextmanager
-from fastapi import FastAPI
-
-logger = logging.getLogger(__name__)
 
 ml_model = {}
 
@@ -30,40 +26,69 @@ app = FastAPI(lifespan=lifespan, debug=True)
 
 @app.get("/wordcloud")
 async def get_wordcloud():
-    
-    # Get the word cloud buffer
-    img_buffer = eda_utils.create_wordcloud(df)
+    """Craete wordcloud endpoint
 
-    # Return the image as a response
+    Returns:
+        StreamingResponse: a streaming response containing a wordcloud image
+    """
+    img_buffer = eda_utils.create_wordcloud(df)
     return StreamingResponse(img_buffer, media_type="image/png")
 
 
 @app.get("/label-count")
 async def get_label_count():
+    """Count the labels endpoint
+
+    Returns:
+        StreamingResponse: a streaming response containing a count of labels image
+    """
     img_buffer = eda_utils.plot_label_count(df)
     return StreamingResponse(img_buffer, media_type="image/png")
 
 
 @app.get("/label-correlation")
 async def get_label_correlation():
+    """Label correlation endpoint
+
+    Returns:
+        StreamingResponse: a streaming response containing a label correlation image
+    """
     img_buffer = eda_utils.correlation_between_labels(df)
     return StreamingResponse(img_buffer, media_type="image/png")
 
 
 @app.get("/most-common-mesh")
 async def get_most_common_mesh():
+    """Most common mesh endpoint
+
+    Returns:
+        StreamingResponse: a streaming response containing an image of the most common mesh
+    """
     img_buffer = eda_utils.count_most_common_mesh(df)
     return StreamingResponse(img_buffer, media_type="image/png")
 
 
 @app.get("/text-length-distribution")
 async def get_text_length_distribution():
+    """Text length distribution endpoint
+
+    Returns:
+        Streamingresponse: a streaming response containing an image of text length distribution
+    """
     img_buffer = eda_utils.draw_text_length_distribution(df)
     return StreamingResponse(img_buffer, media_type="image/png")
 
 
 @app.post("/predict")
 async def predict(query: str):
+    """Inference endpoint which predicts the labels an article belongs
+
+    Args:
+        query (str): the input article/query
+
+    Returns:
+        dict: the predicted labels the article belongs
+    """
     inputs = sbert.encode(query, convert_to_tensor=True, device=device)
     output = ml_model["answer"](inputs)
     predictions = (torch.sigmoid(output) > 0.5).float()
